19ae18fbc167b053f605d1828af06a0a35ea1775
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-07 21:30:20 -0500

    finished the complete set up with the dotenv dependecy

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+//this reads from a .env file located at the root folder
+//and merge the key/value
 const server = require("./api/server.js");
 // Make the port be assigned bu the server
 //Heroku will place the port environment on their server.
