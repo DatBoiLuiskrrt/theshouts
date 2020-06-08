0325c4c976e0670892179732b46cc59dac575b2c
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-07 19:38:59 -0500

    Added a dynamic port using .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
-
-const port = 5000;
+// Make the port be assigned bu the server
+//Heroku will place the port environment on their server.
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
